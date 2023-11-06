668721a2570aa7aaa57a82cfa7ae1b4cb53fb58e
tylersmith <tyjsmith1@gmail.com>
2023-11-06 17:58:56 -0600

    post responses

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+// // // Add your code here
+
+function submitData() {
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com",
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        const newHead = document.querySelector("body")
+        newHead.appendChild(document.createElement("p"))
+        newHead.innerHTML = data.id
+    })
+    .catch((error) => {
+        alert('Unauthorized Access')
+
+        const newHead = document.querySelector("body")
+        newHead.appendChild(document.createElement("p"))
+        newHead.innerHTML = error
+    })
+}
